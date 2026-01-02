7a2844816cab9b0a7d1207e0cb44f7ea957e614c
sagartayde77 <s.b.tayde777@gmail.com>
2026-01-02 17:34:27 +0530

    added security groups for VPN and Private EC2

---
+++ terraform/security.tf
@@ -0,0 +1,51 @@
+# Security Group for VPN EC2 (Public)
+resource "aws_security_group" "vpn_sg" {
+  name        = "${var.project_name}-vpn-sg"
+  description = "Security group for VPN EC2 (public)"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH access (temporary)"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # later restrict to your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-vpn-sg"
+  }
+}
+
+#Security Group for App EC2 (Private)
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project_name}-app-sg"
+  description = "Security group for private EC2 (app)"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "SSH only from VPN EC2"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.vpn_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-sg"
+  }
+}
